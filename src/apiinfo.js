9e74505c0e098e69b783aaf0e57786a3c3f959ca
Daniel Vitor <danielsalviano879@gmail.com>
2024-06-11 10:58:38 -0300

    fazendo o js da api e linkando com a tela de perfil e tela inicial (deslogado)

--- src/apiinfo.js
+++ src/apiinfo.js
@@ -1,3 +1,5 @@
+var arr= [];
+
 document.addEventListener("DOMContentLoaded", consumirAPI);
 
         function consumirAPI() {
@@ -13,7 +15,7 @@ document.addEventListener("DOMContentLoaded", consumirAPI);
                 });
         }
 
-        function mostrarDados(data) {
+        function mostrarDados(data) { //acervo
             const apiDataDiv = document.getElementById('api-data');
             apiDataDiv.innerHTML = ''; 
 
@@ -57,14 +59,11 @@ document.addEventListener("DOMContentLoaded", consumirAPI);
                 cardButton.style.marginTop = '10px'
                 
                 cardButton.addEventListener('click', () => {
-
-                    alert(
-                        
-                        `
-ID do Livro: ${data[key].id_livro}
-Autor: ${data[key].autor}
-Gênero: ${data[key].genero} 
-Resumo: ${data[key].resumo}`);
+                    
+                    addItem(data[key].nome, data[key].id_livro, data[key].autor, data[key].genero, data[key].resumo);
+        
+                    
+                    window.location.href = 'telaLivro.html';
                 });
 
                 cardBody.appendChild(cardTitle);
@@ -75,3 +74,191 @@ Resumo: ${data[key].resumo}`);
                 apiDataDiv.appendChild(card);
             });
         }
+
+        document.addEventListener("DOMContentLoaded", consumirAP);
+
+        function consumirAP() { //tela inicial
+            const url = 'https://phaccess.vercel.app/';
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    mostrarDado(data);
+                })
+                .catch(error => {
+                    console.error('Erro ao consumir a API:', error);
+                });
+        }
+       
+        function mostrarDado(data) {
+            const apiDataDiv = document.getElementById('api-datas');
+            apiDataDiv.innerHTML = ''; 
+        
+            for (let i = 0; i < 3; i++) {
+                const key = Object.keys(data)[i]; 
+        
+                const card = document.createElement('div');
+                card.className = 'card col-3';
+
+                card.style.marginLeft = '50px';
+                card.style.marginTop = '30px';
+                card.style.marginBottom = '30px';
+
+                const cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+
+                const cardTitle = document.createElement('h5');
+                cardTitle.className = 'card-title';
+                cardTitle.innerText = data[key].nome;
+
+                const cardImage = document.createElement('img');
+                cardImage.className = 'card-img-top';
+                cardImage.src = 'imgaltura.jpg';
+                cardImage.style.height = '350px';
+                
+
+                const cardContent1 = document.createElement('p');
+                cardContent1.className = 'card-text';
+                cardContent1.innerText = `ID do Livro: ${data[key].id_livro}`;
+
+                const cardContent2 = document.createElement('p');
+                cardContent2.className = 'card-text';
+                cardContent2.innerText = `Autor: ${data[key].autor}`;
+
+                const cardContent3 = document.createElement('p');
+                cardContent3.className = 'card-text';
+                cardContent3.innerText = `Gênero: ${data[key].genero}`;
+
+                const cardContent4 = document.createElement('p');
+                cardContent4.className = 'card-text';
+                cardContent4.innerText = `Resumo: ${data[key].resumo}`;
+        
+                const cardButton = document.createElement('button');
+                cardButton.className = 'btn btn-secondary';
+                cardButton.innerText = 'Mostrar Detalhes';
+                cardButton.style.height = '40px';
+                cardButton.style.marginTop = '10px';
+                        
+                cardButton.addEventListener('click', () => {
+                    addItem(data[key].nome, data[key].id_livro, data[key].autor, data[key].genero, data[key].resumo);
+        
+                    
+                    window.location.href = 'telaDePerfil.html';
+                });
+        
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardImage);
+                cardBody.appendChild(cardButton);
+                card.appendChild(cardBody);
+                        
+                apiDataDiv.appendChild(card);
+            }
+        }
+
+        document.addEventListener("DOMContentLoaded", consumirAPIS);
+
+        function consumirAPIS() {
+            const url = 'https://phaccess.vercel.app/';
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    mostrarDadosPerfil(data);
+                })
+                .catch(error => {
+                    console.error('Erro ao consumir a API:', error);
+                });
+        }
+       
+      
+
+        function mostrarDadosPerfil() { //telaperfil
+            const apiDataDiv = document.getElementById('api-dates');
+            apiDataDiv.innerHTML = '';
+        
+            const storedData = localStorage.getItem('meuArr'); 
+            if (storedData) {
+                const data = JSON.parse(storedData);
+        
+                data.forEach(item => {
+                    const card = document.createElement('div');
+                    card.className = 'card col-3';
+                    card.style.marginLeft = '50px';
+                    card.style.marginTop = '30px';
+                    card.style.marginBottom = '30px';
+        
+                    const cardBody = document.createElement('div');
+                    cardBody.className = 'card-body';
+        
+                    const cardTitle = document.createElement('h5');
+                    cardTitle.className = 'card-title';
+                    cardTitle.innerText = item.nome; 
+        
+                    const cardImage = document.createElement('img');
+                    cardImage.className = 'card-img-top';
+                    cardImage.src = 'imgaltura.jpg';
+                    cardImage.style.height = '350px';
+        
+                    const cardContent1 = document.createElement('p');
+                    cardContent1.className = 'card-text';
+                    cardContent1.innerText = `ID do Livro: ${item.id_livro}`;
+        
+                    const cardContent2 = document.createElement('p');
+                    cardContent2.className = 'card-text';
+                    cardContent2.innerText = `Autor: ${item.autor}`;
+        
+                    const cardContent3 = document.createElement('p');
+                    cardContent3.className = 'card-text';
+                    cardContent3.innerText = `Gênero: ${item.genero}`;
+        
+                    const cardContent4 = document.createElement('p');
+                    cardContent4.className = 'card-text';
+                    cardContent4.innerText = `Resumo: ${item.resumo}`;
+        
+                    cardBody.appendChild(cardTitle);
+                    cardBody.appendChild(cardImage);
+                    cardBody.appendChild(cardContent1);
+                    cardBody.appendChild(cardContent2);
+                    cardBody.appendChild(cardContent3);
+                    cardBody.appendChild(cardContent4);
+                    card.appendChild(cardBody);
+        
+                    apiDataDiv.appendChild(card);
+                });
+            }
+        }
+        
+        window.onload = mostrarDadosPerfil;
+        
+        function addItem(nome, id_livro, autor, genero, resumo) {
+            let arr = [];
+        
+            if (localStorage.meuArr) {
+                arr = JSON.parse(localStorage.getItem('meuArr'));
+            }
+        
+    
+            const livroExistente = arr.find(item => item.id_livro === id_livro);
+        
+            if (!livroExistente) {
+                let novoItem = {
+                    nome: nome,
+                    id_livro: id_livro,
+                    autor: autor,
+                    genero: genero,
+                    resumo: resumo
+                };
+        
+                arr.push(novoItem);
+        
+                localStorage.setItem('meuArr', JSON.stringify(arr));
+            } else {
+                
+            }
+        }
+        
+
+      
+        
+        
+        
