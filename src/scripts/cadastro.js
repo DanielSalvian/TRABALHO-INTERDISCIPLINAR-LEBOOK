965b8761d6b26863e7b4738bf47148d4e6fb7368
Gustavo Perdigão <gustavoaugustoperdigao@gmail.com>
2024-06-12 17:30:11 -0300

    Criação da função de cadastro, alteração da ref dos scripts.

--- src/scripts/cadastro.js
+++ src/scripts/cadastro.js
@@ -1,33 +1,29 @@
 function enviarDados() {
-  const nome = document.getElementById('nomeLivro').value;
-  const cpf = document.getElementById('autorLivro').value;
-  const resumo = document.getElementById('resumoLivro').value;
-  const genero = document.getElementById('generoLivro').value;
+  const nome = document.getElementById('nomeCadastro').value;
+  const cpf = document.getElementById('cpfCadastro').value;
+  const email = document.getElementById('emailCadastro').value;
+  const senha = document.getElementById('senhaCadastro').value;
 
-  if (nome && autor && resumo && genero) {
-    fetch('https://phaccess.vercel.app', {
+  if (nome && cpf && email && senha) {
+    fetch('https://phaccess.vercel.app/usuarioLivro', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         nome: nome,
-        autor: autor,
-        resumo: resumo,
-        genero: genero
+        cpf: cpf,
+        email: email,
+        senha: senha
       })
     })
       .then(response => response.json())
       .then(data => {
         alert(data.message);
-        get();
-        document.getElementById('nomeLivro').value = "";
-        document.getElementById('autorLivro').value = "";
-        document.getElementById('resumoLivro').value = "";
-        document.getElementById('generoLivro').value = "";
+        window.location.href='telaLogado.html'
       })
       .catch(error => console.error('Erro ao enviar os dados:', error));
   } else {
-    alert("Preencha o nome, autor, resumo e genero para adicionar!");
+    alert("Preencha corretamente todos os campos!");
   }
 }
