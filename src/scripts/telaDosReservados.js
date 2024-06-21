c4fe6bce9185810937867741203f397e60019ebb
YagoArruda <yago4444@gmail.com>
2024-06-21 00:34:28 -0300

    Função de devolução

--- src/scripts/telaDosReservados.js
+++ src/scripts/telaDosReservados.js
@@ -8,6 +8,8 @@ function mostrarReservas() {
 
     let url = `https://phaccess.vercel.app/emprestimo?email=${usuarioAtual.email}`;
 
+    document.getElementById("livros").innerHTML = `<h3>Nenhum emprestimo!</h3>`; 
+
     fetch(url, {
         method: 'GET',
         headers: {
@@ -16,8 +18,8 @@ function mostrarReservas() {
     })
         .then(response => response.json())
         .then(async data => {
-            let livros = ``;
-
+            let livros = `<h3>Nenhum emprestimo!</h3>`;
+            
             for (i = 0; i < data.length; i++) {
                 livros += `<div class="card-livro">
         <img src="data:image/png;base64,${data[i].capa}" alt="Nome do livro" class="capa-livro">
@@ -26,15 +28,15 @@ function mostrarReservas() {
           <p>Devolução: ${formatarData(data[i].data)} </p>
           <p>Situação: ${calcularDevolucao(data[i].data)}</p>
           <div>
-            <button class="btn-padrao">Devolver</button>
+            <button class="btn-padrao" onclick="devolverLivro(${data[i].id_livro},${usuarioAtual.email})">Devolver</button>
           </div>
         </div>
       </div>`;
                 calcularDevolucao(data[i].data);
             }
 
-
             document.getElementById("livros").innerHTML = livros;
+
         })
         .catch(error => console.error('Erro ao enviar os dados:', error));
 }
@@ -63,7 +65,7 @@ function calcularDevolucao(dataEmprestimo) {
     return estado;
 }
 
-function formatarData(dataEmprestimo){
+function formatarData(dataEmprestimo) {
     partes = dataEmprestimo.split('-')
     ano = partes[0]
     mes = partes[1]
@@ -71,4 +73,24 @@ function formatarData(dataEmprestimo){
 
 
     return `${parseInt(dia) + 15}-${mes}-${ano}`;
+}
+
+function devolverLivro(id, email) {
+    fetch(`https://phaccess.vercel.app/emprestimo`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            email: email
+        })
+    })
+        .then(response => response.json())
+        .then(async data => {
+            alert("Livro devolvido!");
+            document.getElementById("livros").innerHTML = ``;
+            mostrarReservas();
+        })
+        .catch(error => console.error('Erro ao enviar os dados:', error));
 }
